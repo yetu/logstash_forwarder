d7138869858540c3e56b1ae9be19b58a388402c0
Mario Camou <mcamou@users.noreply.github.com>
2014-11-28 17:26:34 +0100

    Removed dependency on sensu_spec

--- metadata.rb
+++ metadata.rb
@@ -7,6 +7,6 @@ description      'Installs/Configures logstash_forwarder'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.5.0'
 
-depends 'sensu_spec', '~> 0.10.0'
+#depends 'sensu_spec', '~> 0.10.0'
 depends 'apt', '>= 2.6'
 depends 'yum', '>= 3.0'
